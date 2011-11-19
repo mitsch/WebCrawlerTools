d3c1f5a72e5f123ba6f194a618a3199d9b39aa58
Michael Koch <mitsch.koch@gmail.com>
2011-11-18 22:35:52 -0500

    completed script

--- filterAttributes.hs
+++ filterAttributes.hs
@@ -4,45 +4,31 @@ module Main where
 	import System.Console.GetOpt
 	import Data.String.Utils
 	import Data.Bool
-
-	{--
-	 - gets all attributes in attr from document
-	 -
-	 - getAttributes (html document) (attribute key) (attribute values)
-	--}
-	getAttributes :: [String] -> [String] -> [String]
-	getAttributes document attrs = concat $ map (getAttributesFromOpenTag attrs) (filter isTagOpen (parseTags document)) where
-		getAttributesFromOpenTag attrs tag = filter (not. null) (map (\attr -> fromAttrib attr tag) attrs)
-	
-	{--
-	 - filters out all links which matches the prefixes or suffixes on the to reject list, but if at least one prefix or suffix matches
-	 - a link the link won't be filtered out regardless of the to-reject lists
-	 -
-	 - filterLinks (prefixes to accept) (prefixes to reject) (suffixes to accept) (suffixes to reject) (links to filter) (links not filtered out)
-	--}
-	filterLinks :: [String] -> [String] -> [String] -> [String] -> [String] -> [String]
-	filterLinks aps rps ass rss links = filter (\l -> and $ or [matchPrefix aps, not. matchPrefix rps] ++ or [matchSuffix ass, not. matchSuffix rss]) links 
-		where	matchPrefix prefixes link = any $ map (\p -> startswith p link) prefixes
-					matchSuffix suffixes link = any $ map (\s -> endswith s link) suffixes
+	import IO
+	import qualified System.Exit as SE
 
 	data Flag = AcceptPrefix String | RejectPrefix String | AcceptSuffix String | RejectSuffix String |
 							AcceptPrefixFile FilePath | RejectPrefixFile FilePath | AcceptSuffixFile FilePath | RejectSuffixFile FilePath |
 							Help
+
+	isHelpFlag :: Flag -> Bool
+	isHelpFlag Help = True
+	isHelpFlag _ = False
 
 	type FixCollection = ([String], [String], [String], [String], [FilePath], [FilePath], [FilePath], [FilePath], [Flag])
 
 	collectFixFlags :: [Flag] -> FixCollection
 	collectFixFlags flags = aux flags [] [] [] [] [] [] [] [] [] where
 		aux (flag:flags) ap rp as rs apf rpf asf rsf unknown = case flag of
-			AcceptPrefix pre -> aux flags pre:ap rp as rs apf rpf asf rsf unknown
-			RejectPrefix pre -> aux flags accPre pre:rejPre asf rsf apf rpf asf rsf unknown
-			AcceptSuffix suf -> aux flags ap rp suf:as rs apf rpf asf rsf unknown
-			RejectSuffix suf -> aux flags ap rp as suf:rs apf rpf asf rsf unknown
-			AcceptPrefixFile file -> aux flags ap rp as rs file:apf rpf asf rsf unknown
-			RejectPrefixFile file -> aux flags ap rp as rs apf file:rpf asf rsf unknown
-			AcceptSuffixFile file -> aux flags ap rp as rs apf rpf file:asf rsf unknown
-			RejectSuffixFile file -> aux flags ap rp as rs apf rpf asf file:rsf unknown
-			_ -> aux flags ap rp asf rsf apf rpf asf rsf flag:unknown
+			AcceptPrefix pre -> aux flags (pre:ap) rp as rs apf rpf asf rsf unknown
+			RejectPrefix pre -> aux flags ap (pre:rp) asf rsf apf rpf asf rsf unknown
+			AcceptSuffix suf -> aux flags ap rp (suf:as) rs apf rpf asf rsf unknown
+			RejectSuffix suf -> aux flags ap rp as (suf:rs) apf rpf asf rsf unknown
+			AcceptPrefixFile file -> aux flags ap rp as rs (file:apf) rpf asf rsf unknown
+			RejectPrefixFile file -> aux flags ap rp as rs apf (file:rpf) asf rsf unknown
+			AcceptSuffixFile file -> aux flags ap rp as rs apf rpf (file:asf) rsf unknown
+			RejectSuffixFile file -> aux flags ap rp as rs apf rpf asf (file:rsf) unknown
+			_ -> aux flags ap rp asf rsf apf rpf asf rsf (flag:unknown)
 		aux [] ap rp as rs apf rpf asf rsf unknown = (ap, rp, as, rsf, apf, rpf, asf, rsf, unknown)
 
 	getAcceptPrefixes :: FixCollection -> [String]
@@ -51,7 +37,7 @@ module Main where
 	getRejectPrefixes :: FixCollection -> [String]
 	getRejectPrefixes (_,rp,_,_,_,_,_,_,_) = rp
 
-	getAcceptSuffixes :::FixCollection -> [String]
+	getAcceptSuffixes :: FixCollection -> [String]
 	getAcceptSuffixes (_,_,as,_,_,_,_,_,_) = as
 
 	getRejectSuffixes :: FixCollection -> [String]
@@ -72,29 +58,18 @@ module Main where
 	getUnknownFlags :: FixCollection -> [Flag]
 	getUnknownFlags (_,_,_,_,_,_,_,_,unknown) = unknown
 
-	data OptCompResult = ExecFilter { acceptPrefixes :: [String],
-																	  rejectPrefixes :: [String],
-																		acceptSuffixes :: [String],
-																		rejectSuffixes :: [String],
-																		acceptPrefixFiles :: [FilePath],
-																		rejectPrefixFiles :: [FilePath],
-																		acceptSuffixFiles :: [FilePath],
-																		rejectSuffixFiles :: [FilePath] } |
-												DisplayHelp |
-												ErrorOcurred [String]
-
 	options :: [OptDescr Flag]
 	options = [Option [] ["accept-prefix", "ap"] (ReqArg (\str -> AcceptPrefix str) "PREFIX") "accept always links having this prefix",
 						 Option [] ["reject-prefix", "rp"] (ReqArg (\str -> RejectPrefix str) "PREFIX") "reject links having this prefix",
 						 Option [] ["accept-suffix", "as"] (ReqArg (\str -> AcceptSuffix str) "SUFFIX") "accept always links having this suffix",
 						 Option [] ["reject-suffix", "rs"] (ReqArg (\str -> RejectSuffix str) "SUFFIX") "reject links having this suffix",
-						 Option [] ["accept-prefix-file", "apf"] (ReqArgs (\file -> AcceptPrefixFile file) "FILE")
+						 Option [] ["accept-prefix-file", "apf"] (ReqArg (\file -> AcceptPrefixFile file) "FILE")
 						 	"file contains list of prefixes (one per line) to accept",
-             Option [] ["reject-prefix-file", "rpf"] (ReqArgs (\file -> RejectPrefixFile file) "FILE")
+             Option [] ["reject-prefix-file", "rpf"] (ReqArg (\file -> RejectPrefixFile file) "FILE")
 						 	"file contains list of prefixes (one per line) to reject",
-						 Option [] ["accept-suffix-file", "asf"] (ReqArgs (\file -> AcceptSuffixFile file) "FILE")
+						 Option [] ["accept-suffix-file", "asf"] (ReqArg (\file -> AcceptSuffixFile file) "FILE")
 						 	"file contains list of suffixes (one per line) to accept",
-						 Option [] ["reject-suffix-file", "rsf"] (ReqArgs (\file -> RejectSuffixFile file) "FILE")
+						 Option [] ["reject-suffix-file", "rsf"] (ReqArg (\file -> RejectSuffixFile file) "FILE")
 						 	"file contains list of suffixes (one per line) to reject",
 						 Option ['h'] ["help"] (NoArg Help) "prints out description"]
 
@@ -108,21 +83,50 @@ module Main where
 			(opts,_,[]) -> Exec opts
 			(_,_,errors) -> Error errors
 
+	isError :: OptionsCompileResult -> Bool
+	isError (Error _) = True
+	isError _ = False
 
+	getErrorFromResult :: OptionsCompileResult -> [String]
+	getErrorFromResult (Error errors) = errors
+	getErrorFromResult _ = error "wrong argument"
 
+	getFlags :: OptionsCompileResult -> [Flag]
+	getFlags (Exec flags) = flags
+	getFlags _ = error "wrong argument"
 
-	main = do
-		args <- getArgs
-		contents <- getContents
-		let fixes = collectFixFlags args in 
+	main = do {
+						; arguments <- getArgs
+						; let compileResult = compileOptions arguments
+						; if isError compileResult
+								then do {
+												; mapM_ putStrLn (getErrorFromResult compileResult ++ [usageInfo "usage: filterAttributes [options]" options])
+												; SE.exitWith $ SE.ExitFailure 1
+												}
+								else return ()
+						; let fixes = collectFixFlags $ getFlags compileResult
+						; if any isHelpFlag (getUnknownFlags fixes)
+								then do {
+												; putStrLn (usageInfo "usage: filterAttributes [options]" options)
+												; SE.exitWith $ SE.ExitFailure 0
+												}
+								else return ()
+						; acceptPrefixes <- mapM getFixesFromFile (getAcceptPrefixFiles fixes)
+						; rejectPrefixes <- mapM getFixesFromFile (getRejectPrefixFiles fixes)
+						; acceptSuffixes <- mapM getFixesFromFile (getAcceptSuffixFiles fixes)
+						; rejectSuffixes <- mapM getFixesFromFile (getRejectSuffixFiles fixes)
+						; contents <- getContents
+						; let attributes = executeFilter (concat acceptPrefixes ++ getAcceptPrefixes fixes) (concat rejectPrefixes ++ getRejectPrefixes fixes) (concat acceptSuffixes ++ getAcceptSuffixes fixes) (concat rejectSuffixes ++ getRejectSuffixes fixes) ["href", "src"] contents
+						; mapM_ putStrLn attributes
+						}
 	
-	case compileOptions arguments of
-		Error errors = putStrLn. usageInfo "filterArguments [options ...]" arguments
-		Exec flags = mapMap_ putStrLn (getAttributes () ())
+	executeFilter :: [String] -> [String] -> [String] -> [String] -> [String] -> String -> [String]
+	executeFilter acceptPrefixes rejectPrefixes acceptSuffixes rejectSuffixes attrFilters document = let
+		attrs = filter (not. null) (concat $ map (\tag -> map (\filter -> fromAttrib filter tag) attrFilters) (filter isTagOpen (parseTags document)))
+		in filterAttributes acceptPrefixes rejectPrefixes acceptSuffixes rejectSuffixes attrs
 
-	main = getArgs >>= (\arguments -> case compileOptions arguments of
-		Error errors = putStrLn. usageInfo "filterArguments [options ...]" arguments
-		Exec flags = let rp = rejectPrefixs
-		mapM_ putStrLn (filterLinks (rejectPrefixs append ) () () () (getAttributes contents ["href", "src"])))
-
-		mapM_ putStrLn (filterLinks acceptPrefixes rejectPrefixes acceptSuffixes rejectSuffixes (getAttributes contents ["href", "src"]))
+	filterAttributes :: [String] -> [String] -> [String] -> [String] -> [String] -> [String]
+	filterAttributes acceptPrefixes rejectPrefixes acceptSuffixes rejectSuffixes attributes = let
+		matchPrefix prefixes attribute = or $ map (\prefix -> startswith prefix attribute) prefixes
+		matchSuffix suffixes attribute = or $ map (\suffix -> endswith suffix attribute) suffixes
+		in filter (\attribute -> and [or [matchPrefix acceptPrefixes attribute, not $ matchPrefix rejectPrefixes attribute], or [matchSuffix acceptSuffixes attribute, not $ matchSuffix rejectSuffixes attribute]]) attributes
